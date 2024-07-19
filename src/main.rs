17cd16e4bf433c8f9d5302aa73e0ef8ae86d4e1b
jg <jeremy51b5@pm.me>
2024-07-19 16:50:42 -0400

    init push to repo

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder}; // webapp framework
+use actix_web::middleware::Logger; // logging middleware (requests)
+use env_logger::Env; // logging
+use tera::Tera; // html templating engine
+use lazy_static::lazy_static; //ensure templates only initialized once.
+
+// lazy_static is used to ensure that the templates are only initialized once.
+lazy_static! {
+    pub static ref TEMPLATES: Tera = {
+        // let source = "templates/**/*";
+        let tera = match Tera::new("templates/**/*") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing Error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+        tera
+    };
+ }
+
+
+#[get("/")]
+async fn index() -> impl Responder {
+    let context = tera::Context::new();
+    let page_content = TEMPLATES.render("index.html", &context).unwrap();
+    HttpResponse::Ok().body(page_content)
+}
+
+
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> { 
+    let _server_logs = env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    HttpServer::new(|| 
+        App::new()
+            .wrap(Logger::default())
+            .service(index)
+    )
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
