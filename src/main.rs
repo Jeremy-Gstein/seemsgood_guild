964add0d0c82818867d785ceee9776834bdc351b
jg <j51b5@pm.me>
2024-07-20 22:51:42 -0400

    added http form processing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder}; // webapp framework
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result}; // webapp framework
 use actix_web::middleware::Logger; // logging middleware (requests)
 use env_logger::Env; // logging
 use tera::Tera; // html templating engine
 use lazy_static::lazy_static; //ensure templates only initialized once.
+use serde::{Serialize, Deserialize}; // json parsing
 
 // lazy_static is used to ensure that the templates are only initialized once.
 lazy_static! {
@@ -20,6 +21,31 @@ lazy_static! {
  }
 
 
+
+// form scope
+struct AppState {
+    state: String,
+}
+
+
+#[derive(Serialize, Deserialize)]
+pub struct FormInput {
+    name: String,
+}
+
+
+async fn guild_application() -> impl Responder {
+    let context = tera::Context::new();
+    let page_content = TEMPLATES.render("form.html", &context).unwrap();
+    HttpResponse::Ok().body(page_content)
+}
+
+async fn postup(params: web::Form<FormInput>) -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/plain")
+        .body(format!("Your name is {}", params.name)))
+}
+
 #[get("/")]
 async fn index() -> impl Responder {
     let context = tera::Context::new();
@@ -34,12 +60,26 @@ async fn index() -> impl Responder {
 async fn main() -> std::io::Result<()> { 
     let _server_logs = env_logger::init_from_env(Env::default().default_filter_or("info"));
 
-    HttpServer::new(|| 
+    HttpServer::new(||  
         App::new()
             .wrap(Logger::default())
             .service(index)
+            .service(web::resource("/form").route(web::get().to(guild_application)))
+            .service(web::resource("/postup").route(web::post().to(postup)))
+
     )
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
+
+
+
+
+
+
+
+
+
+
+
