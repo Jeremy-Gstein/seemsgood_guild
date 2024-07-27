4740a70ac718ce9aecca7ad8a4a759dbe80e8c40
jg <jeremy51b5@pm.me>
2024-07-26 20:56:09 -0400

    generate user id with Uuidv4

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result}; // webapp framework
 use actix_web::middleware::Logger; // logging middleware (requests)
-use env_logger::Env; // logging
 use tera::Tera; // html templating engine
 use lazy_static::lazy_static; //ensure templates only initialized once.
 use serde::{Serialize, Deserialize}; // json Parsing
+use uuid::Uuid;                                    // uuid
 
 // lazy_static is used to ensure that the templates are only initialized once.
 lazy_static! {
@@ -21,9 +21,15 @@ lazy_static! {
  }
 
 
+struct User {
+    id: String,
+    name: String,
+    realm: String,
+}
+
+
 #[derive(Serialize, Deserialize)]
 pub struct FormInput {
-    id: i32,
     name: String,
     realm: String,
 }
@@ -37,7 +43,20 @@ async fn guild_application() -> impl Responder {
 
 #[post("/postup")]
 async fn postup(params: web::Form<FormInput>) -> Result<HttpResponse> {
-    println!("\n[POST-DATA] - Id:{:?} Name:{:?} Realm:{:?}\n",params.id, params.name, params.realm);
+    println!("\n[POST-DATA] - Name:{:?} Realm:{:?}\n",params.name, params.realm);
+    let name = &params.name;
+    let realm = &params.realm;
+    // gererate a new UUID for user
+    let id = Uuid::new_v4().to_string();
+    // extract forminput data and create a new user.
+    // currently printing the data to stdout.
+    // this will be stored in a db, or sent directly to discord api.
+    let user = User {
+        id: id.clone(),
+        name: name.clone(),
+        realm: realm.clone(),
+    };
+    println!("[Application] - ID:{:?}, Name:{:?}, Realm:{:?}\n", user.id, user.name, user.realm);
     Ok(HttpResponse::Ok()
         .content_type("text/plain")
         .body("You've submitted the form!"))
