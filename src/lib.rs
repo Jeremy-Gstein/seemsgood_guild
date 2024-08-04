bed81bc1d61594b603b9936d9b412a46c7cc1d65
jg <j51b5@pm.me>
2024-08-04 18:14:04 -0400

    add templateing support using askama_axum

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,12 @@ use axum::{routing::get, Router};
 use tower_service::Service;
 use worker::*;
 use axum::response::Html;
+use include_dir::{include_dir, Dir};
+use askama_axum::Template;
+
+static _TEMPLATES_DIR: Dir = include_dir!("templates");
+
+
 
 
 
@@ -21,14 +27,31 @@ async fn fetch(
     Ok(router().call(req).await?)
 }
 
-async fn homepage() -> Html<&'static str> {
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate;
+
+async fn homepage() -> Html<String> {
     // `std::include_str` macro can be used to include an utf-8 file as `&'static str` in compile
     // time. This method is relative to current `main.rs` file.
-    Html(include_str!("../templates/layout.html"))
+//    let _ = TEMPLATES_DIR;
+    let template = IndexTemplate;
+    let rendered = template.render().unwrap();
+    
+    Html(rendered)
 }
 
-async fn applypage() -> Html<&'static str> {
+#[derive(Template)]
+#[template(path = "apply.html")]
+struct ApplyTemplate;
+
+
+
+
+async fn applypage() -> Html<String> {
     // `std::include_str` macro can be used to include an utf-8 file as `&'static str` in compile
     // time. This method is relative to current `main.rs` file.
-    Html(include_str!("../templates/apply.html"))
+    let template = ApplyTemplate;
+    let rendered = template.render().unwrap();
+    Html(rendered)
 }
