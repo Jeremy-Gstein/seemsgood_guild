c5ffe314afc65e62b83f395ef2ba9d2a4c299a1e
Jeremy-Gstein <jeremy51b5@pm.me>
2025-03-27 16:11:59 -0400

    update mythic key roster

--- src/mythic_plus.rs
+++ src/mythic_plus.rs
@@ -51,18 +51,25 @@ struct RaidFramesTemplate {
     players: Vec<Player>,
 }
 
+
+/* TODO use this api to get roster - 
+ * curl -X 'GET' https://wowaudit.com/v1/characters -H 'accept: application/json' -H 'Authorization: c210cab6ac2405faca8e60e0514a94fddede5015902f9e52ec401a7a2086d970' |  jq '.[] | select(.rank == "Officer" or .rank == "Raider") | {name, realm, class, rank}'
+*/
+
 pub async fn mythicplus_page() -> axum::response::Html<String> {
     // Creating the list of players, including class RGB
     let players = vec![
+        // tanks
         Player { name: "Jtusendh", class: PlayerClass::DemonHunter, realm: "Stormrage" },
         Player { name: "Paliduh", class: PlayerClass::Paladin, realm: "Stormrage" },
+        // healers
         Player { name: "Delusionol", class: PlayerClass::Priest, realm: "Stormrage" },
         Player { name: "Evelianne", class: PlayerClass::Monk, realm: "Stormrage" },
         Player { name: "Pipmeow", class: PlayerClass::Druid, realm: "Tichondrius" },
         Player { name: "Auraelia", class: PlayerClass::Priest, realm: "Zul'jin" },
         Player { name: "Caael", class: PlayerClass::Paladin, realm: "Zul'jin" },
+        // damage 
         Player { name: "Infilicious", class: PlayerClass::Mage, realm: "Stormrage" },
-        // out for season Player { name: "Bigtittyrog", class: PlayerClass::Druid, realm: "Stormrage" },
         Player { name: "Hekthuzad", class: PlayerClass::Mage, realm: "Stormrage" },
         Player { name: "Indico", class: PlayerClass::Evoker, realm: "Zul'jin" },
         Player { name: "Ladöra", class: PlayerClass::Priest, realm: "Stormrage" },
@@ -70,12 +77,10 @@ pub async fn mythicplus_page() -> axum::response::Html<String> {
         Player { name: "Notshodo", class: PlayerClass::Evoker, realm: "Stormrage" },
         Player { name: "Kolzane", class: PlayerClass::Hunter, realm: "Ysera" },
         Player { name: "Speara", class: PlayerClass::Druid, realm: "Kel'Thuzad" },
-        //out for season Player { name: "Vinnea", class: PlayerClass::Shaman, realm: "Kel'Thuzad" },
         Player { name: "Nyanslok", class: PlayerClass::Warlock, realm: "Stormrage" },
         Player { name: "Dubshamm", class: PlayerClass::Shaman, realm: "Stormrage" },
         Player { name: "Chuubers", class: PlayerClass::Warrior, realm: "Stormrage" },
-        Player { name: "Crsete", class: PlayerClass::Rogue, realm: "Tichondrius" },
-        //out for season Player { name: "Crypticist", class: PlayerClass::DeathKnight, realm: "Zul'jin" },
+        Player { name: "Conncrete", class: PlayerClass::Rogue, realm: "Tichondrius" },
         Player { name: "Filio", class: PlayerClass::Monk, realm: "Stormrage" },
         Player { name: "Jakksparrow", class: PlayerClass::Paladin, realm: "Stormrage" },
         Player { name: "Juukmonk", class: PlayerClass::Monk, realm: "Zul'jin" },
