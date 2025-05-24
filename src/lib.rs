bb749776449adf394993706d79d0bf719338a6e2
Jeremy-Gstein <jeremy51b5@pm.me>
2025-05-24 19:29:18 -0400

    testing google adsense as possible revenue for  monthly wowaudit subscription

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,7 @@ fn router() -> Router {
         .route("/dps-sims", get(dps_sims::damagesimspage))
         .route("/keys",  get(mythic_plus::mythicplus_page))
         .route("/wowaudit", get(wowaudit_page))
+        .route("/ads.txt", get(ads_page))
         .fallback(Redirect::permanent("/"))
 }
 
@@ -82,3 +83,23 @@ async fn wowaudit_page() -> Html<String> {
     let rendered = template.render().unwrap();
     Html(rendered)
 }
+
+
+// this route is for Google Adsense.
+// i hate ads..
+// this is an expirament to see if $3 a month can ge generated from ads.
+// currently Wowaudit - used for scraping data about the guild in sg_assist repo has a service fee
+// of $3 per month. I really like the service and want to sustainably use it.
+// i sincerally appologise for moving this burdon onto the users of this site.
+// in return i pledge to find better ways of securing funding and if any surpluse is achived here
+// it will only go towards bettering the quality of this site and other projects.
+#[derive(Template)]
+#[template(path = "ads.txt")]
+struct AdsTemplate;
+
+async fn ads_page() -> Html<String> {
+    let template = AdsTemplate;
+    let rendered = template.render().unwrap();
+    Html(rendered)
+}
+
