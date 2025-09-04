f57974509777d9681ec6f7f0c65e997a405b7498
Jeremy-Gstein <jeremy51b5@pm.me>
2025-09-04 02:04:02 -0400

    scritpt for updating and pushing events to repo

--- get-events.sh
+++ get-events.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/bash
 
 WOWAUDIT_TOKEN=$1
-ASSET_PATH="$PWD/templates/assets/"
-GIT_REPO=""
-REPO_NAME=""
+ASSET_PATH="/tmp/seemsgood_guild/templates/assets/"
+GIT_REPO="https://github.com/Jeremy-Gstein/seemsgood_guild"
+REPO_NAME="seemsgood_guild"
+
+git remote set-url origin git@github.com:Jeremy-Gstein/seemsgood_guild
 # Get the latest events.json from wowaudit and save to templates/assets/events.json
-curl -X 'GET' \
-  'https://wowaudit.com/v1/historical_data' \
-  -H 'accept: application/json' \
-  -H 'Authorization: '$WOWAUDIT_TOKEN > "$ASSET_PATH/events.json"
+get_events() {
+  curl -X 'GET' \
+    'https://wowaudit.com/v1/historical_data' \
+    -H 'accept: application/json' \
+    -H 'Authorization: '$WOWAUDIT_TOKEN > "$ASSET_PATH/events.json"
+}
 
 # upload to git.
 get_repo() {
   cd /tmp
-  git clone $GIT_REPO && cd $REPO_NAME && \
+  git clone $GIT_REPO && cd $REPO_NAME 
+  get_events && \
     git add ./templates/assets/events.json && \
     git commit -am "Update events.json" && \
+    git remote set-url origin git@github.com:Jeremy-Gstein/seemsgood_guild && \
     git push
 }
+
+get_repo
 
 # diff the current events.json and check if its actually populated.
